8a30ca2569bdcb93ea32f02b89d8f1b8f97d46c7
Rotem_Kalir <rotem.kalir@post.runi.ac.il>
2024-12-08 23:35:00 +0100

    shopping_cart - add_item is using str of item and added first version of remove_item

--- shopping_cart.py
+++ shopping_cart.py
@@ -6,14 +6,16 @@ class ShoppingCart:
         self.items = {}
 
     def add_item(self, item: Item):
-        if item in self.items:
-            raise errors.ItemAlreadyExistsError("Item {} already exists".format(item))
+        if item in self.items: #  item name already exists in the shopping cart
+            raise errors.ItemAlreadyExistsError("Item {} already exists".format(str(item))
         else:
-            self.items[item.name]= item
+            self.items[item.name]= item # add item to shopping cart
 
     def remove_item(self, item_name: str):
-        # TODO: Complete
-        pass
+        if item_name not in self.items: # no item with the given name exists
+            raise errors.ItemNotExistError("Item {} doesn't exist".format(item_name))
+        else:
+            del self.items[item_name] # remove item from the shopping cart
 
     def get_subtotal(self) -> int:
         # TODO: Complete

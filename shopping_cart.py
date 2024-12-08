b716c7a85d46f27e4b0f201e8318f59312b175a9
Rotem_Kalir <rotem.kalir@post.runi.ac.il>
2024-12-08 23:22:17 +0100

    shopping_cart - constructor and add_item first version

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,10 +1,15 @@
 from item import Item
-
+import errors
 
 class ShoppingCart:
+    def __init__(self): #constructor
+        self.items = {}
+
     def add_item(self, item: Item):
-        # TODO: Complete
-        pass
+        if item in self.items:
+            raise errors.ItemAlreadyExistsError("Item {} already exists".format(item))
+        else:
+            self.items[item.name]= item
 
     def remove_item(self, item_name: str):
         # TODO: Complete

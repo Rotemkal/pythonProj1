d407ff96ac06128b7cbf5da7e51afc5b0914c2d6
Rotem_Kalir <rotem.kalir@post.runi.ac.il>
2024-12-10 00:38:35 +0100

    store - added comments to _sort_by_hashtag and added first version of add_item

--- store.py
+++ store.py
@@ -2,6 +2,7 @@ from email.policy import default
 
 import yaml
 
+import errors
 from item import Item
 from shopping_cart import ShoppingCart
 
@@ -40,8 +41,17 @@ class Store:
         return searchlst
 
     def add_item(self, item_name: str):
-        # TODO: Complete
-        pass
+        count = 0
+        for item in self._items: # checks how many items name matches item_name
+            if item_name in item.name:
+                count +=1
+        if count == 1: # only one item matches' adds it to shopping cart,
+            # add_item raises ItemAlreadyExistsError if needed
+            self._shopping_cart.add_item(self._shopping_cart.items(item_name))
+        elif count > 1: # multiple items matching, raises TooManyMatchesError
+            raise errors.TooManyMatchesError("There are multiple items matching the given name")
+        else: # no such item exists
+            raise errors.ItemNotExistError("There is no item with the given name")
 
     def remove_item(self, item_name: str):
         # TODO: Complete
@@ -52,20 +62,20 @@ class Store:
         pass
 
     def _sort_by_hashtag(self, items_list: list) -> list:
-        hashtag_list = []
-        for item in self._shopping_cart.items:
+        hashtag_list = [] # building a new hash list
+        for item in self._shopping_cart.items: # building list of all hashtags of all items in current shopping cart
             hashtag_list += item.hashtags
-        hash_dict = {}
-        for item in items_list:
+        hash_dict = {} # dict for items and their num of matching to hashtag_list
+        for item in items_list: # counting matches for each item
             hash_count = 0
             for hashtag in hashtag_list:
                 if hashtag in item.hashtags:
                     hash_count += 1
-            hash_dict[item] = hash_count
+            hash_dict[item] = hash_count # adding to dict
         item_hash_lst = list(hash_dict.items()) # get list of tuples : (item,count)
-        item_hash_lst.sort(key=lambda item: item[0].name) # handling equal counters' sorts by lexical order
-        item_hash_lst.sort(key=lambda item : item[1])
-        sort_lst = []
-        for item in item_hash_lst:
+        item_hash_lst.sort(key=lambda item: item[0].name) # first sort by names, handling equal counters
+        item_hash_lst.sort(key=lambda item : item[1]) # sort by counts
+        sort_lst = [] # building the sort list
+        for item in item_hash_lst: # dict is sort, appends to list by order
             sort_lst.append(item[0])
         return sort_lst
